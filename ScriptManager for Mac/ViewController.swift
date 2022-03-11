9783d9e7c7adb0fc51757dbe67dd5209aff8e4b6
Jason Jeon <john40644064@gmail.com>
2022-03-11 19:05:53 +0900

    Initial Commit

---
+++ ScriptManager for Mac/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  ScriptManager for Mac
+//
+//  Created by Jason Jeon on 3/11/22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
